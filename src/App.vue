531b40500ab7e4221423516ce99b388f80325372
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-09-05 16:27:07 -0300

    feat: Iniciando implementação de middleware service para bloquear rotas não atenticadas.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Login</router-link> |
-    <router-link to="/register">Register</router-link>
+    <router-link to="/register">Register</router-link> |
+    <router-link to="/about">About</router-link>
   </nav>
   <router-view />
 </template>
